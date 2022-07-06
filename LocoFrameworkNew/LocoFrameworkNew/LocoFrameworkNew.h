9da55cc04add70ce7fb0b93ff3e69a72f3f70c49
mayank bhawsar <mayank.bhawsar@5exceptions.com>
2022-07-06 18:04:20 +0530

    Finished our framework

---
+++ LocoFrameworkNew/LocoFrameworkNew/LocoFrameworkNew.h
@@ -0,0 +1,18 @@
+//
+//  LocoFrameworkNew.h
+//  LocoFrameworkNew
+//
+//  Created by iOS_Mayank on 06/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocoFrameworkNew.
+FOUNDATION_EXPORT double LocoFrameworkNewVersionNumber;
+
+//! Project version string for LocoFrameworkNew.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkNewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFrameworkNew/PublicHeader.h>
+
+
---
+++ LocoFrameworkNew/LocoFrameworkNew/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  LocoFrameworkNew
+//
+//  Created by iOS_Mayank on 06/07/22.
+//
+
+import Foundation
+
+public class Service{
+    private init(){}
+    public static func doSomething() -> String  {
+        return "do something"
+    }
+}
+  
